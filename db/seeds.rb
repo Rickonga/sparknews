6e75e5ff6640e6acdd2ad34ff739cf9f91a96c0e
Benedikt Richter <richter-benedikt@t-online.de>
2020-05-20 16:57:21 +0200

    moved stocks to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+url = "https://finnhub.io/api/v1/stock/symbol?exchange=US&token=br2g2ufrh5rbm8ou31m0"
+stocks = JSON.parse(open(url).read)
+stocks.each do |e|
+  Stock.create!(name: e["description"], ticker: e["symbol"]) unless e["description"].empty? || e["symbol"].empty?
+end
+
+puts "finished Stocks"
