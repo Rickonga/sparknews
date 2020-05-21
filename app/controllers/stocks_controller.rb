b50120c9591bd32e1580f1e2cd0afe4dd5ab3961
“Riccardo <riccardoongaro@live.com>
2020-05-21 17:04:16 +0200

    Twitter part one

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,27 @@
+require 'open-uri'
+
 class StocksController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    client = set_twitter
+    #@timeline = client.home_timeline
+    # client.search("#appl -rt", lang: "en")
+    # first.text
+
+    @post = client.search("#amazon -rt", lang: "en").first
+    # @results = []
+    # client.search("#amazon -rt", lang: "en").first do |post|
+    #   @results << post.text
+    # end
+    # topics = ["apple", "amazon"]
+    # results = []
+    # client.filter(track: topics.join(",")) do |object|
+    #   results << object.text if object.is_a?(Twitter::Tweet)
+    # end
+    # @post3 = results
+
+
     resolution = 1
     start_time = 1572651390
     end_time = 1572910590
@@ -16,6 +36,16 @@ class StocksController < ApplicationController
   end
 
   private
+
+  def set_twitter
+    client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["CONSUMER_KEY"]
+    config.consumer_secret     = ENV["CONSUMER_SECRET"]
+    config.access_token        = ENV["ACCESS_TOKEN"]
+    config.access_token_secret = ENV["ACCESS_SECRET"]
+    end
+    client
+  end
 
   def use_stock_api(ticker, stock, resolution, start_time, end_time)
     # stock.quotes.destroy_all
--- db/schema.rb
+++ db/schema.rb
@@ -94,7 +94,6 @@ ActiveRecord::Schema.define(version: 2020_05_21_095427) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
