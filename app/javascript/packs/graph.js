fed7d8d2f1e37e12346956bc4be8e82b271a02e5
Victor Noual <victor.noual@orange.fr>
2020-05-21 23:32:11 +0200

    no chartkick, only chart.js, almost flipped out

--- app/javascript/packs/graph.js
+++ app/javascript/packs/graph.js
@@ -1,34 +1,96 @@
 import Chart from 'chart.js';
 
-// const mychart = document.getElementById("lineChart");
-// let lineChart = new Chart(mychart; {
-//  type: 'line',
-//  data: {
- //       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//        datasets: [{
-//            label: '# of Votes',
-//            data: [12, 19, 3, 5, 2, 3],
-//            backgroundColor: "rgba(75,192,192,0.4)",
- //           borderColor: "rgba(75,192,192,1)",
-//            borderCapStyle: 'butt',
-//            borderDash: [],
-//            borderDashOffset: 0.0,
-//            borderJoinStyle: 'miter',
-//            pointBorderColor: "rgba(75,192,192,1)",
-//            pointBackgroundColor: "#fff",
-//            pointBorderWidth: 1,
- //           pointHoverRadius: 5,
- //           pointHoverBackgroundColor: "rgba(75,192,192,1)",
- //           pointHoverBorderColor: "rgba(220,220,220,1)",
-//            pointHoverBorderWidth: 2,
-//            borderWidth: 1,
-//            pointHitRadius: 10,
-//        }]
-//    }
-//});
-//
+let mydata = document.getElementById('lineChart');
+let myquotes = JSON.parse(mydata.dataset.quotes);
+const timeLine = JSON.parse(mydata.dataset.time);
 
-let data_from_api = [{ x: 1, y: 2}, {x: 1.5, y: 1}];
+const mychart = document.getElementById("lineChart");
+let lineChart = new Chart(mychart, {
+  type: 'line',
+  data: {
+    labels: timeLine,
+    datasets: [{
+            label: 'merde',
+            data: myquotes,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            borderWidth: 1,
+            pointHitRadius: 10,
+        }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      }
+    }
+});
+
+/*
+
+var data_from_api = [{ x: 1, y: 2}, {x: 1.5,y: 1}];
+
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myLineChart = new Chart(ctx, {
+   type: 'line',
+   data: [0]
+
+// options: options
+});
 
 
-document.getElementById("lineChart").innerText = "merde"; // .getContext('2d');
+
+
+var ctx = document.getElementById('lineChart');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+      }
+  }
+});
+
+*/
