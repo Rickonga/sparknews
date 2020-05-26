5c7851e22c47cc1f2b15ca1b8a7f4528b004982e
“Riccardo <riccardoongaro@live.com>
2020-05-26 22:40:13 +0200

    Improved the style for the website, plus added the modal for Watchlist

--- db/schema.rb
+++ db/schema.rb
@@ -124,7 +124,6 @@ ActiveRecord::Schema.define(version: 2020_05_26_094921) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
