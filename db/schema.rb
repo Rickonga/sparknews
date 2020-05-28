4564e22cf9ae8e762324377748a2004410855f86
florian heidmeier <florian.heidmeier@gmx.net>
2020-05-28 11:17:11 +0200

    change navbar logo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_05_26_094921) do
 
   # These are extensions that must be enabled in order to support this database
@@ -133,7 +132,6 @@ ActiveRecord::Schema.define(version: 2020_05_26_094921) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
